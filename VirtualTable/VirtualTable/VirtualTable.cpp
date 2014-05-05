1dd4ae430a1b5c3bef54260a1f651c2dcfcbc6e3
zrma <bulbitain@nhnnext.org>
2014-05-05 11:10:40 +0900

    Add VirtualTable.cpp

---
+++ VirtualTable/VirtualTable/VirtualTable.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include <iostream>
+
+class Base
+{
+public:
+	virtual void mfunc()
+	{
+		std::cout << "Base::mfunc() " << std::endl;
+	}
+};
+
+class Derived: public Base
+{
+public:
+	virtual void mfunc()
+	{
+		std::cout << "Derived::mfunc() " << std::endl;
+	}
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	typedef void ( Base::*bfunc )( );
+	typedef void ( Derived::*dfunc )( );
+
+	bfunc bb = &Base::mfunc;
+	dfunc dd = &Derived::mfunc;
+
+	if ( bb == dd )
+	{
+		std::cout << "called?" << std::endl;
+	}
+
+	Derived d;
+	( d.*bb )( );
+
+	getchar();
+	return 0;
+}
+
